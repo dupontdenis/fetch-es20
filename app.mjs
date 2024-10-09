c29a7e19938b0d2225db3cbb69989bf905a3b41d
Super Dupont <dupontdenis@users.noreply.github.com>
2024-10-09 13:01:06 +0200

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,5 +1,18 @@
 import { quizz } from "./quizz.mjs";
 
+function escapeHTML(str) {
+  return str.replace(/[&<>'"]/g, (tag) => {
+    const charsToReplace = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      "'": "&#39;",
+      '"': "&quot;",
+    };
+    return charsToReplace[tag] || tag;
+  });
+}
+
 function render(quizz) {
   if (!quizz) {
     throw "The questions list is not available";
@@ -7,16 +20,19 @@ function render(quizz) {
 
   const list = quizz.results
     .map((question) => {
-      return `<li data-answer="${question.correct_answer}">${question.question} </li>`;
+      return `<li data-answer="${escapeHTML(
+        question.correct_answer
+      )}">${escapeHTML(question.question)}</li>`;
     })
     .join("");
 
   return `<ol>${list}</ol>`;
 }
 
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 try {
   container.innerHTML = render(quizz);
 } catch (error) {
   container.innerHTML = error;
+  console.error(error);
 }
